3b0f2ecb8959d2e2b0978d4267181957480ffb3a
jw <jwjwhappy92@gmail.com>
2021-06-12 01:16:26 +0900

    Link 태그 적용

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import "./App.css";
-import MainPage from "./main/index.js";
+import MainPageComponent from "./main/index.js"; ///index.js 부분 생략가능
+import { Switch, Route } from "react-router-dom";
+import UploadPage from "./upload";
+import ProductPage from "./product";
 
 function App() {
-  return <MainPage />;
+  return (
+    <div>
+      <Switch>
+        <Route exact={true} path="/">
+          <MainPageComponent />
+        </Route>
+        <Route exact={true} path="/product/:id">
+          <ProductPage />
+        </Route>
+        <Route exact={true} path="/upload">
+          <UploadPage />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
