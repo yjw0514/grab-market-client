a9b2b185141b78a7e3778b83419a9475d098e4fd
jw <jwjwhappy92@gmail.com>
2021-06-22 19:06:42 +0900

    antd 기본적용

--- src/main/index.js
+++ src/main/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./index.css";
 import axios from "axios";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function MainPage() {
   const [products, setProducts] = React.useState([]);
@@ -21,43 +21,39 @@ function MainPage() {
 
   return (
     <div>
-      <div id="header">
-        <div id="header-area">
-          <img src="images/icons/logo.png" alt="" />
-        </div>
+      <div id="banner">
+        <img src="images/banners/banner1.png" alt="" />
       </div>
-      <div id="body">
-        <div id="banner">
-          <img src="images/banners/banner1.png" alt=""/>
-        </div>
-        <h1>판매되는 상품들</h1>
-        <div id="product-list">
-          {products.map(function (product, index) {
-            return (
-              <div className="product-card">
-                <Link className="product-link" to={`/products/${index}`}>
-                  <div>
-                    <img className="product-img" src={product.imageUrl} alt="" />
+      <h1>판매되는 상품들</h1>
+      <div id="product-list">
+        {products.map(function (product, index) {
+          return (
+            <div className="product-card">
+              <Link 
+                style={{color: "inherit"}}
+                className="product-link" 
+                to={`/products/${product.id}`}
+              >
+                <div>
+                  <img className="product-img" src={product.imageUrl} alt="" />
+                </div>
+                <div className="product-contents">
+                  <span className="product-name">{product.name}</span>
+                  <span className="product-price">{product.price}원</span>
+                  <div className="product-seller">
+                    <img
+                      className="product-avatar"
+                      src="images/icons/avatar.png"
+                      alt=""
+                    />
+                    <span>{product.seller}</span>
                   </div>
-                  <div className="product-contents">
-                    <span className="product-name">{product.name}</span>
-                    <span className="product-price">{product.price}원</span>
-                    <div calssName="product-seller">
-                      <img
-                        className="product-avatar"
-                        src="images/icons/avatar.png"
-                        alt=""
-                      />
-                      <span>{product.seller}</span>
-                    </div>
-                  </div>
-                </Link>
-              </div>
-            );
-          })}
-        </div>
+                </div>
+              </Link>
+            </div>
+          );
+        })}
       </div>
-      <div id="footer"></div>
     </div>
   );
 }
--- src/product/index.js
+++ src/product/index.js
@@ -1,8 +1,47 @@
 import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { directive } from "@babel/types";
+import "./index.css";
 
 function ProductPage() {
   const { id } = useParams();
-  return <h1>상품 상세 페이지 {id} 상품 </h1>;
+  const [product, setProduct] = useState(null);
+
+  useEffect(function () {
+    axios
+      .get(
+        `https://8fd59b07-a542-4d2f-9852-320116fc4953.mock.pstmn.io/products/${id}`
+      )
+      .then(function (result) {
+        setProduct(result.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }, []);
+
+  if (product === null) {
+    return <h1>상품 정보를 받고 있습니다....</h1>;
+  }
+
+  return (
+    <div>
+      <div id="image-box">
+        <img src={"/" + product.imageUrl} />
+      </div>
+      <div id="profile-box">
+        <img src="/images/icons/avatar.png" />
+        <span>{product.seller}</span>
+      </div>
+      <div id="contents-box">
+        <div id="name">{product.name}</div>
+        <div id="price">{product.price}원</div>
+        <div id="createdAt">2020년 12월 8일</div>
+        <div id="description">{product.description}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ProductPage;
